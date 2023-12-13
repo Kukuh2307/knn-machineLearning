1fce48d2275ee726f7a950163ec9e089578555b4
kukuh2307 <kukuhagung12@gmail.com>
2023-12-13 23:03:52 +0700

    modify) model

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 
 # Model = '/home/kisawa16/Documents/KULIAH/MACHINE LEARNING/UTS/KNN/knn_model.pkl'
-knnModel = pickle.load(open('knn_model.pkl', 'rb'))
+knnModel = pickle.load(open('knn_model.sav', 'rb'))
 
 st.title('KNN - Prediksi Kualitas Air')
 
